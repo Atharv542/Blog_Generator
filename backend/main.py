8570f467d5a5b852a3671e9d900b279575624b14
Atharv542 <162612781+Atharv542@users.noreply.github.com>
2026-03-21 22:07:25 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://blog-generator-one-lac.vercel.app/",
+        "https://blog-generator-one-lac.vercel.app",
         "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
